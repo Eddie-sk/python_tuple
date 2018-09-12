69a5f82d9fb7a01b41d17cebccf298ad3757ce4c
sunkai <596629732@qq.com>
2018-09-12 18:10:06 +0800

    list

--- List/list.py
+++ List/list.py
@@ -1,9 +1,37 @@
-# --coding : utf-8 --
+# -- coding: utf-8 --
 
-arr1 = [1, '23', 'Hello']
+class person(object):
+
+    def __init__(self):
+        self.name = None
+        self.age = None
+        self.gender = None
+
+
+
+arr1 = [1, 23]
 arr1.append(123)
 arr1.extend([4, 5, 6])
 print arr1
-arr1.pop()#删除列表中给定位置的项目，然后将其返回。如果未指定索引，则删除列表中的最后一项
+arr1.pop()  # 删除列表中给定位置的项目，然后将其返回。如果未指定索引，则删除列表中的最后一项
 
-arr1.count('23')#返回列表中'23'出现的次数
+arr1.count(23)  # 返回列表中'23'出现的次数
+
+arr1.sort(cmp=lambda x, y: cmp(y, x), reverse=1)
+print arr1
+
+t = person()
+t.name = '张三'
+t.age = 10
+t.gender = '男'
+print t.__getattribute__(t.name)
+
+t2 = person()
+t2.name = '李四'
+t2.age = 15
+t2.gender = '女'
+
+arr2 = [t, t2]
+
+arr2.sort(cmp=lambda x, y: cmp(x.age, y.age), reverse=False)
+print arr2
